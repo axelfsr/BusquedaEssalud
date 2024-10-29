00f3b4dae02b525be627f45ec08434c8ff2a7d81
Salas Ramos Axel Freddy <axel.salas@essalud.gob.pe>
2024-10-29 15:39:31 -0500

    Resolvió conflictos después del pull

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -14,14 +14,22 @@ export class ApiService {
 
   // Método para el login
   login(usuario: string, password: string): Observable<any> {
+<<<<<<< HEAD
     return this.http.post(`${this.baseUrl}/login`, { usuario, password });
+=======
+    return this.http.post(`${environment.apiUrl}/login`, { usuario, password });
+>>>>>>> ad3c91a (Cambiando El Front)
   }
 
   // Método para obtener sugerencias (con token)
   getSugerencias(columna: string, q: string): Observable<any> {
     const token = localStorage.getItem('token'); // Obtiene el token del localStorage
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+<<<<<<< HEAD
     return this.http.get(`${this.baseUrl}/sugerencias`, { headers, params: { columna, q } });
+=======
+    return this.http.get(`${environment.apiUrl}/sugerencias`, { headers, params: { columna, q } });
+>>>>>>> ad3c91a (Cambiando El Front)
   }
 
   logout() {
@@ -32,7 +40,11 @@ export class ApiService {
   // Método para realizar la búsqueda (con token)
   buscar(columna: string, valor_busqueda: string, token: string): Observable<any> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+<<<<<<< HEAD
     return this.http.post(`${this.baseUrl}/buscar`, { columna, valor_busqueda }, { headers });
+=======
+    return this.http.post(`${environment.apiUrl}/buscar`, { columna, valor_busqueda }, { headers });
+>>>>>>> ad3c91a (Cambiando El Front)
   }
   
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 export const environment = {
     production: false,
+<<<<<<< HEAD
     apiUrl: 'http://localhost:5000' // URL de la API local
+=======
+    apiUrl: 'https://buscaressalud.onrender.com' // URL de la API local
+>>>>>>> ad3c91a (Cambiando El Front)
   };
   
